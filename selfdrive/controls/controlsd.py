d8e400e91049f637c11c8b612152c712cc8ec2e8
nikkurie <fuzitanirenti@yahoo.co.jp>
2020-08-31 12:15:56 +0900

    test

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -438,8 +438,6 @@ class Controls:
 
     alerts = self.events.create_alerts(self.current_alert_types, [self.CP, self.sm, self.is_metric])
     self.AM.add_many(self.sm.frame, alerts, self.enabled)
-      traffic_status = sm['trafficModelEvent'].status
-      traffic_confidence = round(sm['trafficModelEvent'].confidence * 100, 2)
       traffic_status = self.sm['trafficModelEvent'].status
       traffic_confidence = round(self.sm['trafficModelEvent'].confidence * 100, 2)
       if traffic_confidence >= 75:
